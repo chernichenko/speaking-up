ce67af21565d4370702fc09009252b8f74d7de97
chernichenko <alexchernichenko18@gmail.com>
2022-10-27 14:19:52 +0300

    add registration form

--- src/components/Form/Button/Button.tsx
+++ src/components/Form/Button/Button.tsx
@@ -5,18 +5,20 @@ import styles from './Button.module.scss'
 
 interface IButtonProps {
     readonly children: React.ReactNode
+    readonly className?: string
     readonly isPrimary?: boolean
     readonly onClick?: () => void
     readonly disabled?: boolean
     readonly type?: "button" | "submit" | "reset" | undefined
 }
 
-export const Button: FC<IButtonProps> = ({ children, isPrimary = true, onClick, disabled, type = 'button' }) => {
+export const Button: FC<IButtonProps> = ({ children, className, isPrimary = true, onClick, disabled, type = 'button' }) => {
     return (
         <button
-            className={cn(styles.button, {
+            className={cn(styles.button, className, {
                 [styles.primary]: isPrimary,
                 [styles.secondary]: !isPrimary,
+                [styles.disabled]: disabled,
             })}
             onClick={onClick}
             disabled={disabled}
