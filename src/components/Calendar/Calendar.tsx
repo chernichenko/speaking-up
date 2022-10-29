dba54a87a2b6d57462801092c0501b561add4c95
chernichenko <alexchernichenko18@gmail.com>
2022-10-29 17:30:33 +0300

    fix

--- src/components/Calendar/Calendar.tsx
+++ src/components/Calendar/Calendar.tsx
@@ -6,18 +6,36 @@ import styles from './Calendar.module.scss'
 
 const DAYS_OF_WEEK = ['MON', 'TUE', 'WED', 'THU', 'FRI', 'SAT', 'SUN']
 const TIMES = ['', '1 AM', '2 AM', '3 AM', '4 AM', '5 AM', '6 AM', '7 AM', '8 AM', '9 AM', '10 AM', '11  AM', '12 PM', '1 PM', '2 PM', '3 PM', '4 PM', '5 PM', '6 PM', '7 PM', '8 PM', '9 PM', '10 PM', '11  PM']
+const MINUTES_IN_HOUR = 60
+const ROW_HEIGHT = 40
 
 export const Calendar = () => {
   const [days, setDays] = useState<number[]>([])
-  const today = new Date()
+  const [currentDay, setCurrentDay] = useState<number>()
+  const [currentTime, setCurrentTime] = useState<any>({  })
 
   useEffect(() => {
+    const today = new Date()
+    setCurrentDay(today.getDate())
+    setCurrentTime({ hours: today.getHours(), minutes: today.getMinutes() })
+
     const newDays = DAYS_OF_WEEK.map((_, index) => {
       const date = new Date(today.setDate(today.getDate() - today.getDay() + index + 1))
       return date.getDate()
     })
     setDays(newDays)
   }, [])
+
+  useEffect(() => {
+    if (days?.length && currentDay && currentTime) {
+      const timeLine: any = document.querySelector('#timeLine')
+      if (timeLine) {
+        const minutes = currentTime.minutes
+        const top = minutes / MINUTES_IN_HOUR * ROW_HEIGHT;
+        timeLine.style.top = `${top}px`;
+      }
+    }
+  }, [days, currentDay, currentTime])
 
   return (
     <div className={styles.wrap}>
@@ -27,10 +45,18 @@ export const Calendar = () => {
           <div className={styles.top}>
             <div className={cn(styles.column, styles.first)} />
             {DAYS_OF_WEEK.map((day, index) => {
+              const isActiveColumn = currentDay === days[index]
               return (
                 <div
                   key={index}
-                  className={cn(styles.column, today.getDate() === days[index] && styles.active, index + 1 === DAYS_OF_WEEK.length && styles.last)}>
+                  className={
+                    cn(
+                      styles.column,
+                      isActiveColumn && styles.activeColumn,
+                      index + 1 === DAYS_OF_WEEK.length && styles.last
+                    )
+                  }
+                >
                   <div className={styles.day}>{day}</div>
                   <div className={styles.numberOfDay}>{days[index]}</div>
                 </div>
@@ -38,15 +64,38 @@ export const Calendar = () => {
             })}
           </div>
           <div className={styles.main}>
-            {TIMES.map((time, index) => {
+            {TIMES.map((time, rowIndex) => {
+              const activeRow = currentTime.hours === rowIndex
               return (
-                <div key={index} className={styles.row}>
+                <div
+                  key={rowIndex}
+                  className={
+                    cn(
+                      styles.row,
+                      activeRow && styles.activeRow,
+                    )
+                  }
+                >
                   <div className={cn(styles.column, styles.first)}>
                     <div className={styles.time}>{time}</div>
                   </div>
-                  {DAYS_OF_WEEK.map((_, index) => {
+                  {DAYS_OF_WEEK.map((_, columndIndex) => {
+                    const activeColumn = currentDay === days[columndIndex]
                     return (
-                      <div key={index} className={cn(styles.column, index + 1 === DAYS_OF_WEEK.length && styles.last)} />
+                      <div
+                        key={columndIndex}
+                        className={
+                          cn(
+                            styles.column,
+                            activeColumn && styles.activeColumn,
+                            columndIndex + 1 === DAYS_OF_WEEK.length && styles.last
+                          )
+                        }
+                      >
+                        {(activeRow && activeColumn) && (
+                          <div id="timeLine" className={styles.timeLine} />
+                        )}
+                      </div>
                     )
                   })}
                 </div>
